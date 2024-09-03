78e2ccba5bf9275c55cee297fd357a4cc7a367ad
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2024-09-04 03:51:10 +0600

    chore: take year dynamically in footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
-    return (
-      <footer className="bg-gray-800 text-white p-4 text-center">
-        <p>&copy; 2024 Ride Sharing Platform. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <p>
+        &copy; {new Date().getUTCFullYear()} Ride Sharing Platform. All rights
+        reserved.
+      </p>
+    </footer>
+  );
+}
